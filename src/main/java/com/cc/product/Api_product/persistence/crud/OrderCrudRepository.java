d5d04804bc22e138edffbb367957d97b6af53234
CrisFRC <ing.cristian.roac@gmail.com>
2023-05-13 16:44:20 -0500

    creados metodos utiles en la clase ProductRepositoryCrud

--- src/main/java/com/cc/product/Api_product/persistence/crud/OrderCrudRepository.java
+++ src/main/java/com/cc/product/Api_product/persistence/crud/OrderCrudRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 public interface OrderCrudRepository extends CrudRepository<Order,Integer> {
 
     List<Order> findByDateOrderByIdOrderAsc(LocalDateTime date); //get an order list by date (utils for today)
-    Optional<Order> findByIdClientOrderByDate( int idClient, boolean state );//get an order using id client and date and verify state is true
+    Optional<List<Order>> findByIdClientOrderByDate( int idClient, boolean state );//get an order using id client and date and verify state is true
 }
--- src/main/java/com/cc/product/Api_product/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/cc/product/Api_product/persistence/crud/ProductCrudRepository.java
@@ -3,8 +3,12 @@ package com.cc.product.Api_product.persistence.crud;
 import com.cc.product.Api_product.persistence.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductCrudRepository extends CrudRepository<Product,Integer> {
 
-
+    List<Product> findByIdCategoryOrderByNameAsc(int idCategory);
+    Optional<List<Product>> findByStockNumberLessThanAndState(int stockNumber, boolean state);
 
 }
