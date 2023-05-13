50bc33a23c435210438f3c57d86dd972f835c1db
CrisFRC <ing.cristian.roac@gmail.com>
2023-05-13 16:26:48 -0500

    creadas clases iniciales para crud repository de productos y ordenes

---
+++ src/main/java/com/cc/product/Api_product/persistence/crud/ProductCrudRepository.java
@@ -0,0 +1,10 @@
+package com.cc.product.Api_product.persistence.crud;
+
+import com.cc.product.Api_product.persistence.entity.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductCrudRepository extends CrudRepository<Product,Integer> {
+
+    
+
+}
