bec6bcdcc8929905e7e375ec51f1ca921a591190
CrisFRC <ing.cristian.roac@gmail.com>
2023-05-13 16:41:35 -0500

    creados algunos metodos utiles en la clase OrderRepositoryCrud

--- src/main/java/com/cc/product/Api_product/persistence/crud/OrderCrudRepository.java
+++ src/main/java/com/cc/product/Api_product/persistence/crud/OrderCrudRepository.java
@@ -3,9 +3,12 @@ package com.cc.product.Api_product.persistence.crud;
 import com.cc.product.Api_product.persistence.entity.Order;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface OrderCrudRepository extends CrudRepository<Order,Integer> {
 
-
-
-
+    List<Order> findByDateOrderByIdOrderAsc(LocalDateTime date); //get an order list by date (utils for today)
+    Optional<Order> findByIdClientOrderByDate( int idClient, boolean state );//get an order using id client and date and verify state is true
 }
--- src/main/java/com/cc/product/Api_product/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/cc/product/Api_product/persistence/crud/ProductCrudRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ProductCrudRepository extends CrudRepository<Product,Integer> {
 
-    
+
 
 }
